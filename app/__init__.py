f79055047d067cd49be494a7a5eb6e098679c173
erllypaguntalan <e.paguntalan@klab.com>
2016-02-11 14:44:51 +0800

    Added Moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 lm.login_view = 'login'
