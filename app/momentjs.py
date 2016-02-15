a87792991ebf9a17b27bc293420d16f4c7148bab
erllypaguntalan <e.paguntalan@klab.com>
2016-02-15 11:56:06 +0800

    Fixed moment.js bug

--- app/momentjs.py
+++ app/momentjs.py
@@ -1,13 +1,13 @@
 from jinja2 import Markup
 
 class momentjs(object):
-    def __init__(self, timestamp):
-        self.timestamp = timestamp
+    def __init__(self, date_created):
+        self.date_created = date_created
 
     def render(self, format):
         return Markup(
             "<script>\ndocument.write(moment(\"%s\").%s);\n</script>" %
-            (self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
+            (self.date_created.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
